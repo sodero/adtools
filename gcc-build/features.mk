a3b674f46c42fdd88f5114726a1288fa71d0b473
ola.soder@axis.com <ola.soder@axis.com>
2021-01-03 14:42:34 +0100

    gcc10: Disable threading temporarily.
    Due to missing threading functionality threading needs to be disabled
    for now. For more information, enable and build.

--- gcc-build/features.mk
+++ gcc-build/features.mk
@@ -9,5 +9,11 @@ FEATURES=\
 
 # Check, if major version is greater than or equals to 8
 ifeq ($(shell test $(MAJOR_VERSION) -ge 8; echo $$?), 0)
-FEATURES+=--enable-threads=amigaos --enable-lto
+FEATURES+=--enable-lto
+# Temporarily disable threads when building gcc 10.
+ifeq ($(shell test $(MAJOR_VERSION) -eq 10; echo $$?), 0)
+FEATURES+=--disable-threads
+else
+FEATURES+=--enable-threads=amigaos
+endif
 endif
